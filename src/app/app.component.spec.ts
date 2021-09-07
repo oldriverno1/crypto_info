f09abc7a07364f03da1b47ee29ff5fc1e0474db6
Henry <henrywang@systex.com>
2021-09-07 15:58:27 +0800

    rename project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'cryptoScanner'`, () => {
+  it(`should have as title 'cryptoInfo'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('cryptoScanner');
+    expect(app.title).toEqual('cryptoInfo');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('cryptoScanner app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('cryptoInfo app is running!');
   });
 });
