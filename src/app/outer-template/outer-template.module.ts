bbcb188cf72490f06a9336f0a68a4db7cac046c6
Henry Wang <henrywangsihhung100@gmail.com>
2021-09-14 22:46:46 +0800

    1. added footer.component 2. added attribution for nomics

--- src/app/outer-template/outer-template.module.ts
+++ src/app/outer-template/outer-template.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from '../app-routing.module';
 import { PriceMarqueeComponent } from './price-marquee/price-marquee.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [HeaderComponent, PriceMarqueeComponent],
+  declarations: [HeaderComponent, PriceMarqueeComponent, FooterComponent],
   imports: [CommonModule, AppRoutingModule],
-  exports: [HeaderComponent, PriceMarqueeComponent],
+  exports: [HeaderComponent, PriceMarqueeComponent, FooterComponent],
 })
 export class OuterTemplateModule {}
