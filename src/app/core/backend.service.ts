7cc0ba4f402d4e00497c10b6e2d1c8c3cc446452
Henry <henrywang@systex.com>
2021-09-24 14:05:05 +0800

    optimize error handling in backend.service for too many requests situation of nomics

--- src/app/core/backend.service.ts
+++ src/app/core/backend.service.ts
@@ -1,15 +1,15 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { HttpClient, HttpParams } from '@angular/common/http';
-import { catchError } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http';
+import { catchError, delay, mergeMap, retryWhen, take } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class BackendService {
-  constructor(private httpClient: HttpClient) {}
+  constructor(private httpClient: HttpClient) { }
   private readonly NOMICS_KEY: string = '78f51ac49b5a2f483772daebcf27519e8b59988d';
-
+  private readonly TOO_MANY_REQUESTS: number = 429;
   post<T>(apiSource: API_SOURCE, apiName: string, params: Record<string, unknown>): Observable<T> {
     return this.httpClient.post<T>(apiSource + apiName, params).pipe(
       catchError((err) => {
@@ -28,9 +28,16 @@ export class BackendService {
     return this.httpClient
       .get<T>(apiSource + apiName, { params: params as unknown as HttpParams })
       .pipe(
-        catchError((err) => {
-          alert(`發生網路問題[${apiSource + apiName}]`);
-          throw new Error(`發生網路問題 Error Code: ${err.status}`);
+        retryWhen(observable => {
+          return observable.pipe(
+            mergeMap((error: HttpErrorResponse) => {
+              if (error.status === this.TOO_MANY_REQUESTS) {
+                return of(error).pipe(delay(1000));
+              }
+              console.error(error);
+              throw new Error(`error with api: ${apiSource + apiName}`);
+            }), take(2)
+          );
         })
       );
   }
