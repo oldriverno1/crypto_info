bc0e85ccdab01d2675991a8bc8676ad8ecf4b5e3
Henry Wang <henrywangsihhung100@gmail.com>
2021-09-25 22:39:56 +0800

    1. added interfaces folder 2. move currency-ticker interfaces inside interfaces folder

--- src/app/coin-data/overall-table/overall-table.component.ts
+++ src/app/coin-data/overall-table/overall-table.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { BackendService, API_SOURCE } from 'src/app/core/backend.service';
 import { faCaretDown, faCaretUp, IconDefinition } from '@fortawesome/free-solid-svg-icons';
+import { CurrencyTickersResp, CurrencyTickersRequest } from 'src/app/interfaces/currency-ticker';
 @Component({
   selector: 'app-overall-table',
   templateUrl: './overall-table.component.html',
@@ -38,55 +39,4 @@ export class OverallTableComponent implements OnInit {
   }
 }
 
-export interface IntervalDetail {
-  volume: string;
-  price_change: string;
-  price_change_pct: string;
-  volume_change: string;
-  volume_change_pct: string;
-  market_cap_change: string;
-  market_cap_change_pct: string;
-}
 
-export interface CurrencyTickersResp {
-  id: string;
-  currency: string;
-  symbol: string;
-  name: string;
-  logo_url: string;
-  status: string;
-  price: string;
-  price_date: Date;
-  price_timestamp: Date;
-  circulating_supply: string;
-  max_supply: string;
-  market_cap: string;
-  market_cap_dominance: string;
-  num_exchanges: string;
-  num_pairs: string;
-  num_pairs_unmapped: string;
-  first_candle: Date;
-  first_trade: Date;
-  first_order_book: Date;
-  rank: string;
-  rank_delta: string;
-  high: string;
-  high_timestamp: Date;
-  '1d': IntervalDetail;
-  '7d': IntervalDetail;
-  '30d'?: IntervalDetail;
-  '365d'?: IntervalDetail;
-  ytd: IntervalDetail;
-}
-
-export interface CurrencyTickersRequest {
-  ids?: string;
-  interval?: string;
-  convert?: string;
-  status?: 'active' | 'inactive' | 'dead';
-  filter?: 'any' | 'new';
-  sort?: 'rank' | 'first_priced_at';
-  'include-transparency'?: boolean;
-  'per-page'?: number;
-  page?: number;
-}
---
+++ src/app/interfaces/currency-ticker.ts
@@ -0,0 +1,52 @@
+export interface IntervalDetail {
+  volume: string;
+  price_change: string;
+  price_change_pct: string;
+  volume_change: string;
+  volume_change_pct: string;
+  market_cap_change: string;
+  market_cap_change_pct: string;
+}
+
+export interface CurrencyTickersResp {
+  id: string;
+  currency: string;
+  symbol: string;
+  name: string;
+  logo_url: string;
+  status: string;
+  price: string;
+  price_date: Date;
+  price_timestamp: Date;
+  circulating_supply: string;
+  max_supply: string;
+  market_cap: string;
+  market_cap_dominance: string;
+  num_exchanges: string;
+  num_pairs: string;
+  num_pairs_unmapped: string;
+  first_candle: Date;
+  first_trade: Date;
+  first_order_book: Date;
+  rank: string;
+  rank_delta: string;
+  high: string;
+  high_timestamp: Date;
+  '1d': IntervalDetail;
+  '7d': IntervalDetail;
+  '30d'?: IntervalDetail;
+  '365d'?: IntervalDetail;
+  ytd: IntervalDetail;
+}
+
+export interface CurrencyTickersRequest {
+  ids?: string;
+  interval?: string;
+  convert?: string;
+  status?: 'active' | 'inactive' | 'dead';
+  filter?: 'any' | 'new';
+  sort?: 'rank' | 'first_priced_at';
+  'include-transparency'?: boolean;
+  'per-page'?: number;
+  page?: number;
+}
