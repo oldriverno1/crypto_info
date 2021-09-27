76e4cddfc36811000f290bd16b521825c84eddca
Henry Wang <henrywangsihhung100@gmail.com>
2021-09-27 22:28:44 +0800

    implement template data binding of market-cap.component

--- src/app/coin-data/market-cap/market-cap.component.ts
+++ src/app/coin-data/market-cap/market-cap.component.ts
@@ -1,5 +1,6 @@
 import { API_SOURCE, BackendService } from 'src/app/core/backend.service';
 import { Component, OnInit } from '@angular/core';
+import { CurrencyTickersRequest, CurrencyTickersResp } from 'src/app/interfaces/currency-ticker';
 
 @Component({
   selector: 'app-market-cap',
@@ -8,10 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MarketCapComponent implements OnInit {
   constructor(private backendService: BackendService) {}
-
+  currencyTickersResp!: CurrencyTickersResp;
   ngOnInit(): void {
-
+    const currencyTickersRequest: CurrencyTickersRequest = { ids: 'BTC' };
+    this.backendService
+      .get<CurrencyTickersResp[]>(API_SOURCE.NOMICS, 'currencies/ticker', currencyTickersRequest as Record<string, unknown>)
+      .subscribe((data) => {
+        this.currencyTickersResp = data[0];
+      });
   }
-
-
 }
